d7ef521d261d74ef6811885e190652883cdba88d
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2024-03-15 18:06:04 +0600

    menu item

--- src/Page/Home/PopularItem/PopularItem.jsx
+++ src/Page/Home/PopularItem/PopularItem.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
 import SectionTile from "../../../Componets/sectionTitle/SectionTilte"
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
 
 const PopularItem = () => {
+  const [menus, setMenus] = useState([])
+    useEffect(() => {
+        fetch("menu.json")
+            .then(res => res.json())
+            .then(data => {
+                const popularItem = data?.filter(item => item.filter(item.category === "popular"))
+                setMenus(popularItem)
+        })
+    }, [])
+    console.log(menus,);
     return (
-        <section>
+        <section className="bg-red-500">
             <SectionTile
                 heading={"FROM OUR MENU"}
                 subheading={''}
             ></SectionTile>
-
+            {
+                menus.map(item =>
+                    <MenuItem key={item._id}
+                    item={item}
+                    >
+                    </MenuItem>
+                    )
+            }
         </section>
     );
 };
