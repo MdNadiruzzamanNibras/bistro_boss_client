6b7be096f1c1c2ed03cd08fd6c48c50b8bd5814a
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2024-03-16 12:44:49 +0600

    featured add

--- src/Componets/sectionTitle/SectionTilte.jsx
+++ src/Componets/sectionTitle/SectionTilte.jsx
@@ -1,13 +1,13 @@
-
-
 // eslint-disable-next-line react/prop-types
-const SectionTilte = ({subheading, heading}) => {
-    return (
-        <div className="w-4/12 mb-8 mx-auto text-center">
-            <p className="text-lg text-yellow-600 mb-5">{subheading}</p>
-            <h4 className="border-y-2 py-4 border-gray-300 text-3xl font-semibold">{ heading}</h4>
-        </div>
-    );
+const SectionTilte = ({ subheading, heading }) => {
+  return (
+    <div className="w-4/12 mb-8 mx-auto text-center">
+      <p className="text-lg text-yellow-600 mb-5">{subheading}</p>
+      <h4 className="border-y-2 py-4 border-gray-300 text-3xl font-semibold">
+        {heading}
+      </h4>
+    </div>
+  );
 };
 
 export default SectionTilte;
--- src/Page/Home/Featured/Freatured.jsx
+++ src/Page/Home/Featured/Freatured.jsx
@@ -1,26 +1,36 @@
 import SectionTilte from "../../../Componets/sectionTitle/SectionTilte";
-import featuredImg from "../../../assets/images/home/featured.jpg"
-import './Featured.css'
+import featuredImg from "../../../assets/images/home/featured.jpg";
+import "./Featured.css";
 const Freatured = () => {
-    return (
-        <div className="feature-item ">
-            <SectionTilte
-                heading={"FROM OUR MENU"}
-                 subheading={'---Check it out---'}
-            />
-            <div className="md:flex justify-center items-center px-32 py-16">
-                <div>
-                    <img src={featuredImg} alt="" />
-                </div>
-                <div className="md:ml-10 text-white">
-                    <p>March 20, 2023</p>
-                    <p className="uppercase">WHERE CAN I GET SOME?</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, quae quisquam quam inventore molestiae porro, praesentium explicabo amet neque optio doloribus. Labore ratione perspiciatis consequatur repellendus harum esse facere fugiat voluptatem quisquam magni doloremque optio, at officia praesentium architecto nihil explicabo, obcaecati modi tenetur et accusantium numquam, mollitia odit! Odio.</p>
-                    <button className="btn btn-outline">Read More</button>
-                </div>
-            </div>
+  return (
+    <div className="feature-item bg-fixed pt-8 my-12">
+      <SectionTilte
+        heading={"FROM OUR MENU"}
+        subheading={"---Check it out---"}
+      />
+      <div className="md:flex justify-center items-center bg-slate-400 bg-opacity-40 px-32 py-16">
+        <div>
+          <img src={featuredImg} alt="" />
         </div>
-    );
+        <div className="md:ml-10 text-white">
+          <p>March 20, 2023</p>
+          <p className="uppercase">WHERE CAN I GET SOME?</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatum, quae quisquam quam inventore molestiae porro,
+            praesentium explicabo amet neque optio doloribus. Labore ratione
+            perspiciatis consequatur repellendus harum esse facere fugiat
+            voluptatem quisquam magni doloremque optio, at officia praesentium
+            architecto nihil explicabo, obcaecati modi tenetur et accusantium
+            numquam, mollitia odit! Odio.
+          </p>
+          <button className="btn border-0 border-b-4 border-white btn-outline">
+            Read More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Freatured;
